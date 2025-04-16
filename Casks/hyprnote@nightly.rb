ee4e0cb8c50d61b9325803441198c3b49046fa69
yujonglee <yujonglee.dev@gmail.com>
2025-04-16 11:21:51 +0900

    dmg-aarch64 is actually universal

--- Casks/hyprnote@nightly.rb
+++ Casks/hyprnote@nightly.rb
@@ -1,9 +1,8 @@
 cask "hyprnote@nightly" do
-  arch arm: "aarch64", intel: "x86_64"
   version :latest
   sha256 :no_check
   
-  url "https://cdn.crabnebula.app/download/fastrepl/hyprnote/latest/platform/dmg-#{arch}?channel=nightly"
+  url "https://cdn.crabnebula.app/download/fastrepl/hyprnote/latest/platform/dmg-aarch64?channel=nightly"
   name "Hyprnote Nightly"
   desc "Nightly build of Hyprnote application"
   homepage "https://github.com/fastrepl/hyprnote"
