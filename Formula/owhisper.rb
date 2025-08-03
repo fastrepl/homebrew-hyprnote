451e12e05f4c9d3d4d9d6147d7b0e79b38a94881
Yujong Lee <yujonglee.dev@gmail.com>
2025-08-02 21:22:43 -0700

    wip

--- Formula/owhisper.rb
+++ Formula/owhisper.rb
@@ -2,22 +2,24 @@ class Owhisper < Formula
   desc "OWhisper"
   homepage "https://github.com/fastrepl/hyprnote/tree/main/owhisper"
   version "0.0.1"
-  
-  sha256 :no_check
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/fastrepl/hyprnote/releases/download/owhisper_v#{version}/owhisper-server-aarch64-apple-darwin"
+      sha256 :no_check
     else
       url "https://github.com/fastrepl/hyprnote/releases/download/owhisper_v#{version}/owhisper-server-x86_64-apple-darwin"
+      sha256 :no_check
     end
   end
   
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/fastrepl/hyprnote/releases/download/owhisper_v#{version}/owhisper-server-aarch64-unknown-linux-gnu"
+      sha256 :no_check
     else
       url "https://github.com/fastrepl/hyprnote/releases/download/owhisper_v#{version}/owhisper-server-x86_64-unknown-linux-gnu"
+      sha256 :no_check
     end
   end
   
